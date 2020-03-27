bb2fb575969a655f7d04ead6d67a134feac541a3
Papa-Souhaibou <souhaibou.toure122@gmail.com>
2020-03-27 22:37:33 +0000

    Resolution de l'exo_3 mais rencontre un probleme avec le boutton reset: refuse de reiniialiser le formulaire

--- phpProject/exo_3/exercice_3.php
+++ phpProject/exo_3/exercice_3.php
@@ -1,55 +1,68 @@
 <?php
-    session_start();
     include('includes/exo_3.inc.php');
 ?>
 <div id="form1">
     <form action="#" method="post">
         <div>
             <label for="nbr">Combien de mots</label><br>
-            <input type="text" class="input-field" name="nbr" id="nbr" value="<?= (isset($_SESSION["nbr"])?$_SESSION["nbr"]:'') ?>">
+            <input type="text" class="input-field" name="nbr" id="nbr" value="<?= @$_POST["nbr"] ?>">
         </div>
         <button type="submit" class="btn blue" name="submit">Valider</button>
-        <input type="reset" class="btn red" value="Annuler">
-    </form>
-</div>
-<div id="form2">
-    <form action="#" method="post">
+        <input type="reset" class="btn red" value="Annuler" name="reset">
         <?php
-            if(isset($_POST["submit"])){
-                $_SESSION["submit"] = $_POST["submit"];
+            $nbr = (int)(isset($_POST["submit"])&&!empty($_POST["nbr"]))?$_POST["nbr"]:0;
+            $_POST["error"] = 0;
+            if(isset($_POST["submit"]) || isset($_POST["result"])){
                 $nbr = (int)$_POST["nbr"];
-                $_SESSION["nbr"] = $nbr;
-            } 
-            if($_SESSION["nbr"] > 0 OR isset($_POST["result"])){
-                for ($i=0; $i < $_SESSION["nbr"]; $i++) { 
-                ?>
-                    <label for="<?=($i+1) ?>">Mot N<?=($i+1) ?>
-                    </label>
-                    <div>
-                        <input type="text" name="mot[]" id="<?=($i+1) ?>" class="input-field" value="<?=(isset($_POST["mot"][$i])?$_POST["mot"][$i]:'')?>">
-                    </div>
-                <?php
+                if($nbr > 0){
+                    for ($i=0; $i < $nbr; $i++) { 
+                    ?>
+                        <div class="display-result">
+                            <label for="<?=($i+1) ?>">Mot N<?=($i+1) ?>
+                            <?php
+                                if(isset($_POST["result"])){
+                                    $string = $_POST["mot"][$i];
+                                    $length = getLength($string);
+                                    if(!isThisStringValide($string) || $length > 20 || $string === ""){
+                                        echo '<span class="error">Le mot ne doit pas depasser 20 caractere.</span>';
+                                        $_POST["error"]++;
+                                    }
+                                }
+                            ?>
+                            </label>
+                            <input type="text" name="mot[]" id="<?=($i+1) ?>" class="input-field" value="<?= @$_POST["mot"][$i] ?>">
+                        </div>
+                    <?php
+                    }
+                    echo '<button class="btn green" name="result" id="result">Resultat</button>';
                 }
-                echo '<button class="btn green" name="result" id="result">Resultat</button>';
+            }
+            if(isset($_POST["reset"])){
+                $length = getLength($_POST["mot"]);
+                for ($i=0; $i < $length; $i++) { 
+                    $_POST["mot"][$i] = '';
+                }
             }
         ?>
-        <div id="print">
-            <p>
-                <?php
-                if(isset($_POST["result"])){
-                    $times = 0;
-                    $length = getLength($_POST['mot']);
-                    for ($i=0; $i < $length; $i++) { 
-                        $string = toUpperString($_POST["mot"][$i]);
-                        if(isIn($string,"M")>=0){
-                            $times++;
-                        }
+    <div id="print">
+        <p>
+            <?php
+            if(isset($_POST["result"])){
+                $times = 0;
+                $length = getLength($_POST['mot']);
+                // $length = $nbr;
+                for ($i=0; $i < $length; $i++) { 
+                    $string = toUpperString($_POST["mot"][$i]);
+                    if(isIn($string,"M")>=0 && isThisStringValide($string)){
+                        $times++;
                     }
-                    echo "Vous avez saisi $length Mot(s) dont <span>$times avec la lettre M</span>";
                 }
-                ?>
-            </p>
-        </div>
+                if($_POST["error"] === 0)
+                    echo "Vous avez saisi $length Mot(s) dont <span class='result'>$times avec la lettre M</span>";
+            }
+            ?>
+        </p>
+    </div>
     </form>
 </div>
 
@@ -80,10 +93,22 @@
 .btn.green{
     background-color: green;
 }
+.error{
+    color: red;
+    margin-left: 25px;
+}
+.result{
+    display:inline-block;
+    padding: 10px 5px;
+    border-radius: 5px;
+    color:white;
+    font-weigth: bold;
+    background-color: rgba(26,86,3,0.7);
+    /* height:0px; */
+}
 label{
     display:block;
     width: 50%;
-    padding-left: 10px;
-    margin-bottom: 10px;
+    margin: auto;
 }
 </style>
