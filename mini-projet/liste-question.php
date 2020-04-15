3e4f81958a3e08138bb176c9e4975c133b61d44d
Papa-Souhaibou <souhaibou.toure122@gmail.com>
2020-04-15 14:11:59 +0000

    Creation de la page 'liste-question.php'

---
+++ mini-projet/liste-question.php
@@ -0,0 +1,11 @@
+<div id="liste-question">
+    <form action="" method="post">
+        <div id="question-number">
+            <label for="question-number-field">Nombre de question/Jeu</label>
+            <input type="text" class="question-number-field" id="question-number-field" name="number">
+            <button type="submit" class="btn-valid-number">OK</button>
+        </div>
+        <div id="questions">
+        </div>
+    </form>
+</div>
