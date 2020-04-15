805d2ba146979442518cf3f3367774db09293d13
Papa-Souhaibou <souhaibou.toure122@gmail.com>
2020-04-15 14:09:51 +0000

    Restylisation de la page admin

--- mini-projet/settings.php
+++ mini-projet/settings.php
@@ -1,67 +1,88 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/settings.css">
-    <link rel="stylesheet" href="css/create-compte.css">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        include("menu.php");
-    ?>
-    <div id="container">
-        <div id="blue-topbar">
-            <h1>CREER ET PARAMETRER VOS QUIZZ</h1>
-            <button class="deconnexion">Deconnexion</button>
-        </div>
-        <div id="setting">
-            <div id="profile">
-                <div id="pp">
-                    <p class="circle">
-                        <img src="" alt="" srcset="">
-                    </p>
-                    <div id="user-info">
-                        <h1>AAA</h1>
-                        <h1>BBB</h1>
+<?php
+    session_start();
+    include("database.php");
+    if(isset($_SESSION["login"])){
+        $login = $_SESSION["login"];
+        $is_admins_login_found = false;
+        foreach ($data["admins"] as $admin) {
+            if($admin["login"] === $login){
+                $is_admins_login_found = true;
+                break;
+            }
+        }
+        if($is_admins_login_found){
+?>
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="css/index.css">
+                <link rel="stylesheet" href="css/settings.css">
+                <link rel="stylesheet" href="css/liste-question.css">
+                <title>Document</title>
+            </head>
+            <body>
+                <?php
+                    include("menu.php");
+                ?>
+                <div id="container">
+                    <div id="blue-topbar">
+                        <h1>CREER ET PARAMETRER VOS QUIZZ</h1>
+                        <form action="" method="post">
+                            <button class="deconnexion" name="deconnexion">Deconnexion</button>
+                        </form>
+                        <?php
+                            if(isset($_POST["deconnexion"])){
+                                unset($_SESSION["firstname"]);
+                                unset($_SESSION["lastname"]);
+                                unset($_SESSION["login"]);
+                                unset($_SESSION["avatar"]);
+                                header("Location:index.php");
+                            }
+                        ?>
+                    </div>
+                    <div id="setting">
+                        <div id="profile">
+                            <div id="pp">
+                                <p class="circle">
+                                    <img src="<?= @$_SESSION["avatar"] ?>" alt="Avatar" srcset="">
+                                </p>
+                                <div id="user-info">
+                                    <h1><?= @$_SESSION["firstname"] ?></h1>
+                                    <h1><?= @$_SESSION["lastname"] ?></h1>
+                                </div>
+                            </div>
+                            <div id="setting-items">
+                                <a href="settings.php?page=list-question" class="list">Liste Questions</a>
+                                <a href="settings.php?page=create-admin" class="add">Creer Admin</a>
+                                <a href="settings.php?page=user-list" class="list">Liste joueurs</a>
+                                <a href="settings.php?page=create-questions" class="add">Creer Questions</a>
+                            </div>
+                        </div>
+                        <div id="displays">
+                            <?php
+                                $page = isset($_GET["page"]) ? $_GET["page"] : "home";
+                                if ($page === "list-question") {
+                                    include("liste-question.php");
+                                }
+                                if($page === "create-admin"){
+                                    include("inscription-admin.php");
+                                }
+                                elseif ($page === "user-list") {
+                                    include("user-list.php");
+                                }elseif($page === "create-questions"){
+                                    include("create-question.php");
+                                }
+                                
+                            ?>
+                        </div>
                     </div>
                 </div>
-                <div id="setting-items">
-                    <table>
-                        <tr>
-                            <td><a href="settings.php?page=list-question">Liste Questions</a></td>
-                            <td><a href="settings.php?page=list-question"><img src="img/icones/ic-liste.png" class="list" alt="icone liste"></a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="settings.php?page=create-compte">Creer Admin</a></td>
-                            <td><a href="settings.php?page=create-compte"><img src="img/icones/ic-ajout.png" class="add" alt="icone ajout"></a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="settings.php?page=user-list">Liste joueurs</a></td>
-                            <td><a href="settings.php?page=user-list"><img src="img/icones/ic-liste.png" class="list" alt="icone liste"></a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="settings.php?page=create-questions">Creer Questions</a></td>
-                            <td><a href="settings.php?page=create-questions"><img src="img/icones/ic-ajout.png" class="add" alt="icone ajout"></a></td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-            <div id="displays">
-                <?php
-                    $page = isset($_GET["page"]) ? $_GET["page"] : "home";
-                    if($page === "create-compte"){
-                        include("inscription.php");
-                    }
-                    elseif ($page === "user-list") {
-                        echo "Page Liste user";
-                    }
-                ?>
-            </div>
-        </div>
-    </div>
-    <script src="js/setting-items.js"></script>
-</body>
-</html>
+                <script src="js/setting-items.js"></script>
+            </body>
+            </html>
+<?php
+        }
+    }
+?>
