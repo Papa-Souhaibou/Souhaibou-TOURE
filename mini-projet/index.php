c9d0d8212ab6a68c81d78167571ba0afe65e4a64
Papa-Souhaibou <souhaibou.toure122@gmail.com>
2020-04-26 17:52:20 +0000

    Ajout directe du barre de menu

--- mini-projet/index.php
+++ mini-projet/index.php
@@ -7,12 +7,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="shortcut icon" href="img/logo-QuizzSA.png" type="image/x-icon">
     <title>Document</title>
 </head>
 <body>
-    <?php
-        include("views/menu.php");
-    ?>
+    <header>
+        <nav>
+            <img src="img/logo-QuizzSA.png" alt="logo du Quizz">
+            <h1>Le plaisir de jouer</h1>
+        </nav>
+    </header>
     <div id="container">
         <div id="form">
             <h2>Login Form</h2>
