fac498e4a8957027cb8107652add94fde268fcda
Papa-Souhaibou <souhaibou.toure122@gmail.com>
2020-04-28 18:45:47 +0000

    Creation des fichiers de style et php pour l'affichage des graphs

---
+++ mini-projet/views/admin-dashboard.php
@@ -0,0 +1,11 @@
+<div id="admin-dashboard">
+    <h1>Rendu visuel</h1>
+    <div id="graph-container">
+        <div class="graph">
+            <canvas id="chart-area"></canvas>
+        </div>
+        <div class="graph">
+            <canvas id="pie"></canvas>
+        </div>
+    </div>
+</div>
